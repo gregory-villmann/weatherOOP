ddb9652f9fa9bff58a7024b06e37c35f76a30176
Gregory Villmann <gregory.villmann@khk.ee>
2021-12-02 11:45:54 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// ls obj
+const ls = new LS();
+
+const localStorageCity = ls.getLocationData();
+
+const weather = new Weather(localStorageCity);
+const ui = new UI();
+
+// get city weather
+function getWeather(){
+    weather.getWeather()
+        .then(data =>{
+            ui.drawWeather(data);
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+}
+getWeather();
+
+const form = document.querySelector("#changeCityForm")
+form.addEventListener('submit', changeWeather);
+
+function changeWeather(e){
+    const city = document.querySelector("#changeCityValue").value;
+    weather.changeCity(city);
+    getWeather();
+    ls.setLocationData(city);
+    city.textContent = "";
+    e.preventDefault();
+}
